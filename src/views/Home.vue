76a6599fb9e4c2dd52557ea5b622ca8a52e471d9
Nekrasov-A-A <a.alex.nekrasov@gmail.com>
2021-06-16 00:22:42 +0300

    added 3 project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,21 @@
   <div>
     <StatsPreviewCardComponent />
     <ProjectSecond />
+    <ProfileCardComponent />
   </div>
 </template>
 
 <script>
 import StatsPreviewCardComponent from "../components/StatsPreviewCardComponent/StatsPreviewCardComponent.vue";
 import ProjectSecond from "../components/3-columnPreviewCardComponent/ProjectSecond.vue";
+import ProfileCardComponent from "../components/ProfileCardComponent/ProfileCardComponent.vue";
 
 export default {
   name: "Home",
-  components: { StatsPreviewCardComponent, ProjectSecond },
+  components: {
+    StatsPreviewCardComponent,
+    ProjectSecond,
+    ProfileCardComponent,
+  },
 };
 </script>
